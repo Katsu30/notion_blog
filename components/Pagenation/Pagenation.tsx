37e2263ab8f9e4a917f3a07477261abadc87a4e5
Katsu30 <sc30.katsu@gmail.com>
2023-04-23 21:58:37 +0900

    .feat add pagenation conponent

---
+++ components/Pagenation/Pagenation.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { PAGENATION_COUNT } from '@/constants';
+
+interface Props {
+  numberOfPage?: number;
+  maxPageNum?: number;
+}
+
+export const Pagenation = ({ numberOfPage = 1, maxPageNum = 1 }: Props) => {
+  const getPagenationAdditions = () => {
+    if (numberOfPage - maxPageNum / 2 > 0) {
+      return numberOfPage - Math.floor(PAGENATION_COUNT / 2) + 1;
+    }
+
+    return 1;
+  };
+
+  return (
+    <div className="mb-8 lg:w-1/2 mx-auto rounded-md p-5">
+      <ul className="flex items-center justify-center gap-4">
+        {[...Array(maxPageNum > PAGENATION_COUNT ? 5 : maxPageNum)].map(
+          (_, i) => (
+            <li className="bg-sky-900 rounded-lg w-6 h-8 relative">
+              <Link
+                href={`/page/${i + getPagenationAdditions()}`}
+                className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-gray-100"
+              >
+                {i + getPagenationAdditions()}
+              </Link>
+            </li>
+          )
+        )}
+      </ul>
+    </div>
+  );
+};
